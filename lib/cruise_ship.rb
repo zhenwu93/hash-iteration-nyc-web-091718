d55ab598966164d4158d86dc93eb6576fb5db083
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-14 02:22:35 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,7 +8,7 @@
 
 def select_winner(passengers)
   winner = ""
-  passgengers.each do |suite, name|
+  passengers.each do |suite, name|
     if suite == :suite_a && name.start_with?("A")
       winner = name
     end
